1091af1cec343777a3386ce5631dffd8032fc4d1
Jihye Han <crocus371@gmail.com>
2013-05-22 14:44:54 +0900

    개발 소스 저장소에 반영
    카피엔페이스트 파워~

---
+++ src/dna.java
@@ -0,0 +1,26 @@
+
+public class dna {
+
+	public static void main(String[] args) {
+		
+		char ans[][] = {{'A', 'C', 'G', 'T', 'C', 'G', 'T', 'A', 'A', 'C'},
+						{'G', 'A', 'T', 'A', 'T', 'A', 'G', 'G', 'C', 'A'},
+						{'T', 'T', 'A', 'G', 'C', 'T', 'C', 'A', 'T', 'T'},
+						{'A', 'G', 'C', 'C', 'G', 'G', 'A', 'G', 'A', 'C'}};
+						  
+		int percent = 0;
+		
+			for(int i=0; i<4; i++){
+				for(int j=0; j<10; j++){
+					if(args[j].charAt(0)==ans[i][j])
+						percent+=10;
+					
+				}
+				
+				System.out.println((i+1)+"번째는 " +percent+"%가 일치합니다.");
+				percent = 0;
+			}
+		}
+	}
+
+
